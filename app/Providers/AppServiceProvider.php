f11f0b140955dbb3b0c516760b83c04c086965e9
Diego_Carrasco <diegocarrasco.ordonez@gmail.com>
2026-02-08 18:29:29 -0300

    asdasdasd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Storage;
+
+use League\Flysystem\Filesystem;
+use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter;
+use Google\Cloud\Storage\StorageClient;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +29,23 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        // =========================================
+        // REGISTRO DEL DRIVER GCS (SOLUCIÓN REAL)
+        // =========================================
+        Storage::extend('gcs', function ($app, $config) {
+
+            $storageClient = new StorageClient([
+                'projectId' => $config['project_id'],
+            ]);
+
+            $bucket = $storageClient->bucket($config['bucket']);
+
+            $adapter = new GoogleCloudStorageAdapter($storageClient, $bucket);
+
+            $filesystem = new Filesystem($adapter);
+
+            return new FilesystemAdapter($filesystem, $adapter, $config);
+        });
     }
 }
