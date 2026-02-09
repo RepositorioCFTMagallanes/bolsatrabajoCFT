044ccb1f5e0a0432f7e964b40fa4ea47d3a89555
Diego Carrasco Ordonez <diegocarrascoordonez@MacBook-Air-5.local>
2026-02-09 11:54:24 -0300

    hgasdjahsfas

--- config/app.php
+++ config/app.php
@@ -117,10 +117,19 @@ return [
     | Supported drivers: "file", "cache"
     |
     */
-
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Service Providers
+    |--------------------------------------------------------------------------
+    */
+
+    'providers' => [
+        Spatie\GoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
     ],
 
 ];
