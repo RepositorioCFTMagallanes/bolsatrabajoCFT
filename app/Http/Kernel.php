4d925cb745ad36cf8cda9e188352245d28cffc30
Diego Carrasco Ordonez <diegocarrascoordonez@MacBook-Air-5.local>
2026-02-13 15:17:00 -0300

    Fix Kernel middlewares for file uploads

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,27 +6,22 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * Middleware globales.
-     */
     protected $middleware = [
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     ];
 
-    /**
-     * Grupos de middleware.
-     */
     protected $middlewareGroups = [
         'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
-    /**
-     * Middleware de rutas.
-     */
     protected $routeMiddleware = [
         'auth.custom' => \App\Http\Middleware\AuthCustom::class,
         'role' => \App\Http\Middleware\CheckRole::class,
