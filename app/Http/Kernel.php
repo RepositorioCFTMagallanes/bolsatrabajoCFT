8c91f306c2f0b5fa62bda2325e861caaf65a1519
Diego Carrasco Ordonez <diegocarrascoordonez@MacBook-Air-5.local>
2026-02-13 14:39:27 -0300

    Fix kernel mínimo para manejo de uploads

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,6 +6,27 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+    /**
+     * Middleware globales.
+     */
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    ];
+
+    /**
+     * Grupos de middleware.
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ],
+    ];
+
+    /**
+     * Middleware de rutas.
+     */
     protected $routeMiddleware = [
         'auth.custom' => \App\Http\Middleware\AuthCustom::class,
         'role' => \App\Http\Middleware\CheckRole::class,
